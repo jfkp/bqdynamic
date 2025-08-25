cf45a2b987897f0791062916270bae24724963fd
jfkp <36500849+jfkp@users.noreply.github.com>
2025-08-25 18:04:51 +0200

    Update create_base_table.sql

--- create_base_table.sql
+++ create_base_table.sql
@@ -1,46 +1,78 @@
--- ===============================
--- Managed Iceberg Tables Setup
--- ===============================
+-- Declare the scale variable
 DECLARE SCALE STRING DEFAULT '10G';
-DECLARE DATASET STRING DEFAULT 'blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb';
-DECLARE REGION STRING DEFAULT 'europe-west9';
 
--- List of tables to create
-DECLARE tables ARRAY<STRING> DEFAULT [
-  "store_sales_denorm_start",
-  "store_sales_denorm_upsert",
-  "store_sales_denorm_insert_medium",
-  "store_sales_denorm_delete_medium",
-  "store_sales_denorm_delete_small",
-  "store_sales_denorm_delete_xsmall",
-  "store_sales_denorm_delete_insert_medium",
-  "store_sales_denorm_delete_upsert"
-];
+-- Drop tables
+EXECUTE IMMEDIATE FORMAT('DROP TABLE IF EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.store_sales_denorm_start_%s', SCALE);
+EXECUTE IMMEDIATE FORMAT('DROP TABLE IF EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.store_sales_denorm_delete_medium_%s', SCALE);
+EXECUTE IMMEDIATE FORMAT('DROP TABLE IF EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.store_sales_denorm_delete_small_%s', SCALE);
+EXECUTE IMMEDIATE FORMAT('DROP TABLE IF EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.store_sales_denorm_delete_xsmall_%s', SCALE);
+EXECUTE IMMEDIATE FORMAT('DROP TABLE IF EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.store_sales_denorm_delete_insert_medium_%s', SCALE);
+EXECUTE IMMEDIATE FORMAT('DROP TABLE IF EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.store_sales_denorm_delete_upsert_%s', SCALE);
 
--- ===============================
--- Loop over tables
--- ===============================
-FOR t IN (SELECT * FROM UNNEST(tables) AS table_name) DO
+-- Create tables
+EXECUTE IMMEDIATE FORMAT(
+'CREATE TABLE IF NOT EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.store_sales_denorm_start_%s
+CLUSTER BY ss_sold_date_sk
+WITH CONNECTION `cacib-lsdh-dev-df.europe-west9.bq-co-lsdh-dev-ew9-vai-bench-bl`
+OPTIONS (
+    file_format = ''PARQUET'',
+    table_format = ''ICEBERG'',
+    storage_uri = ''gs://bkt-lsdh-dev-ew9-bench-bl-lakehouse-ext-tb-00/blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb/%s/store_sales_denorm_start''
+) AS
+SELECT * FROM cacib-lsdh-dev-df.bqms_ds_lsdh_dev_ew9_bench_bq_bl_ib_ext_tbl.store_sales_denorm_start_%s;',
+SCALE, SCALE, SCALE);
 
-  -- Drop table if it exists
-  EXECUTE IMMEDIATE FORMAT("""
-    DROP TABLE IF EXISTS `region-%s.%s.%s_%s`
-  """, REGION, DATASET, t.table_name, SCALE);
+EXECUTE IMMEDIATE FORMAT(
+'CREATE TABLE IF NOT EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.store_sales_denorm_upsert_%s
+WITH CONNECTION `cacib-lsdh-dev-df.europe-west9.bq-co-lsdh-dev-ew9-vai-bench-bl`
+OPTIONS (
+    file_format = ''PARQUET'',
+    table_format = ''ICEBERG'',
+    storage_uri = ''gs://bkt-lsdh-dev-ew9-bench-bl-lakehouse-ext-tb-00/blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb/%s/store_sales_denorm_upsert''
+) AS
+SELECT * FROM cacib-lsdh-dev-df.bqms_ds_lsdh_dev_ew9_bench_bq_bl_ib_ext_tbl.store_sales_denorm_upsert_%s;',
+SCALE, SCALE, SCALE);
 
-  -- Create managed Iceberg table
-  EXECUTE IMMEDIATE FORMAT("""
-    CREATE TABLE IF NOT EXISTS `region-%s.%s.%s_%s`
-    OPTIONS (
-      table_format='ICEBERG'
-      %s
-    ) AS
-    SELECT *
-    FROM `region-%s.%s.%s_%s`
-    WHERE 1=0
-  """,
-    REGION, DATASET, t.table_name, SCALE,                   -- target table
-    CASE WHEN t.table_name = "store_sales_denorm_start" THEN "clustering=[ss_sold_date_sk]" ELSE "" END,  -- cluster start table
-    REGION, "bqms_ds_lsdh_dev_ew9_bench_bq_bl_ib_ext_tbl", t.table_name, SCALE  -- source table
-  );
+EXECUTE IMMEDIATE FORMAT(
+'CREATE TABLE IF NOT EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.store_sales_denorm_insert_medium_%s
+WITH CONNECTION `cacib-lsdh-dev-df.europe-west9.bq-co-lsdh-dev-ew9-vai-bench-bl`
+OPTIONS (
+    file_format = ''PARQUET'',
+    table_format = ''ICEBERG'',
+    storage_uri = ''gs://bkt-lsdh-dev-ew9-bench-bl-lakehouse-ext-tb-00/blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb/%s/store_sales_denorm_insert_medium''
+) AS
+SELECT * FROM cacib-lsdh-dev-df.bqms_ds_lsdh_dev_ew9_bench_bq_bl_ib_ext_tbl.store_sales_denorm_insert_medium_%s;',
+SCALE, SCALE, SCALE);
 
-END FOR;
+EXECUTE IMMEDIATE FORMAT(
+'CREATE TABLE IF NOT EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.store_sales_denorm_delete_medium_%s
+WITH CONNECTION `cacib-lsdh-dev-df.europe-west9.bq-co-lsdh-dev-ew9-vai-bench-bl`
+OPTIONS (
+    file_format = ''PARQUET'',
+    table_format = ''ICEBERG'',
+    storage_uri = ''gs://bkt-lsdh-dev-ew9-bench-bl-lakehouse-ext-tb-00/blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb/%s/store_sales_denorm_delete_medium''
+) AS
+SELECT * FROM cacib-lsdh-dev-df.bqms_ds_lsdh_dev_ew9_bench_bq_bl_ib_ext_tbl.store_sales_denorm_delete_medium_%s;',
+SCALE, SCALE, SCALE);
+
+EXECUTE IMMEDIATE FORMAT(
+'CREATE TABLE IF NOT EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.store_sales_denorm_delete_small_%s
+WITH CONNECTION `cacib-lsdh-dev-df.europe-west9.bq-co-lsdh-dev-ew9-vai-bench-bl`
+OPTIONS (
+    file_format = ''PARQUET'',
+    table_format = ''ICEBERG'',
+    storage_uri = ''gs://bkt-lsdh-dev-ew9-bench-bl-lakehouse-ext-tb-00/blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb/%s/store_sales_denorm_delete_small''
+) AS
+SELECT * FROM cacib-lsdh-dev-df.bqms_ds_lsdh_dev_ew9_bench_bq_bl_ib_ext_tbl.store_sales_denorm_delete_small_%s;',
+SCALE, SCALE, SCALE);
+
+EXECUTE IMMEDIATE FORMAT(
+'CREATE TABLE IF NOT EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.store_sales_denorm_delete_xsmall_%s
+WITH CONNECTION `cacib-lsdh-dev-df.europe-west9.bq-co-lsdh-dev-ew9-vai-bench-bl`
+OPTIONS (
+    file_format = ''PARQUET'',
+    table_format = ''ICEBERG'',
+    storage_uri = ''gs://bkt-lsdh-dev-ew9-bench-bl-lakehouse-ext-tb-00/blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb/%s/store_sales_denorm_delete_xsmall''
+) AS
+SELECT * FROM cacib-lsdh-dev-df.bqms_ds_lsdh_dev_ew9_bench_bq_bl_ib_ext_tbl.store_sales_denorm_delete_xsmall_%s;',
+SCALE, SCALE, SCALE);
