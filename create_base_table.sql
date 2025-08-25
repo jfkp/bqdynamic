29531a54e8354232ab66763692c772ab524da0dc
jfkp <36500849+jfkp@users.noreply.github.com>
2025-08-25 14:40:01 +0200

    Update create_base_table.sql

--- create_base_table.sql
+++ create_base_table.sql
@@ -1,4 +1,8 @@
-DECLARE SCALE STRING DEFAULT "10G";  -- change once, reused everywhere
+DECLARE SCALE STRING DEFAULT "10G";  
+DECLARE DATASET STRING DEFAULT "blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb";  
+DECLARE EXT_DATASET STRING DEFAULT "bqms_ds_lsdh_dev_ew9_bench_bq_bl_ib_ext_tbl";  
+DECLARE PROJECT STRING DEFAULT "cacib-lsdh-dev-df";  
+DECLARE BUCKET STRING DEFAULT "bkt-lsdh-dev-ew9-bench-bl-lakehouse-ext-tb-00";
 
 -- List of table suffixes to drop and recreate
 DECLARE table_suffixes ARRAY<STRING>;
@@ -14,24 +18,26 @@ SET table_suffixes = [
 -- Drop loop
 FOR suffix IN (SELECT * FROM UNNEST(table_suffixes)) DO
   EXECUTE IMMEDIATE FORMAT("""
-    DROP TABLE IF EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.%s_%s
-  """, suffix, SCALE);
+    DROP TABLE IF EXISTS %s.%s_%s
+  """, DATASET, suffix, SCALE);
 END FOR;
 
 -- Create loop
 FOR suffix IN (SELECT * FROM UNNEST(table_suffixes)) DO
   EXECUTE IMMEDIATE FORMAT("""
-    CREATE TABLE IF NOT EXISTS blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb.%s_%s
+    CREATE TABLE IF NOT EXISTS %s.%s_%s
     %s
-    WITH CONNECTION `cacib-lsdh-dev-df.europe-west9.bq-co-lsdh-dev-ew9-vai-bench-bl`
+    WITH CONNECTION `%s.europe-west9.bq-co-lsdh-dev-ew9-vai-bench-bl`
     OPTIONS (
       file_format = 'PARQUET',
       table_format = 'ICEBERG',
-      storage_uri ='gs://bkt-lsdh-dev-ew9-bench-bl-lakehouse-ext-tb-00/blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb/%s/%s'
+      storage_uri ='gs://%s/%s/%s'
     ) AS
-    SELECT * FROM cacib-lsdh-dev-df.bqms_ds_lsdh_dev_ew9_bench_bq_bl_ib_ext_tbl.%s_%s
+    SELECT * FROM %s.%s.%s_%s
   """,
-  suffix, SCALE,
-  IF(suffix = "store_sales_denorm_start", "CLUSTER BY ss_sold_date_sk", ""),  -- only start table has CLUSTER
-  SCALE, suffix, suffix, SCALE);
+  DATASET, suffix, SCALE,
+  IF(suffix = "store_sales_denorm_start", "CLUSTER BY ss_sold_date_sk", ""),
+  PROJECT,
+  BUCKET, SCALE, suffix,
+  PROJECT, EXT_DATASET, suffix, SCALE);
 END FOR;
