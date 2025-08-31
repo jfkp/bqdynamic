5b9aff19ac3c5ff153cc67ec99d23a615a0c6cc7
jfkp <36500849+jfkp@users.noreply.github.com>
2025-08-31 12:08:33 +0200

    Update error.py

--- error.py
+++ error.py
@@ -1,23 +1,5 @@
-Traceback (most recent call last):
-  File "c:\Users\U50HO63\PROJET\CDP\BenchMark\bq_read_write_analytics_v8.py", line 946, in <module>
-    df = run_with_local_files("C:\\Users\\U50HO63\\Downloads\\", local_file_dict)
-         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "c:\Users\U50HO63\PROJET\CDP\BenchMark\bq_read_write_analytics_v8.py", line 839, in run_with_local_files
-    generate_plots(combined_df, save_plots=True)
-  File "c:\Users\U50HO63\PROJET\CDP\BenchMark\bq_read_write_analytics_v8.py", line 794, in generate_plots
-    plot_read_write_ratio_analysis(combined_df, save_plots, output_dir)
-  File "c:\Users\U50HO63\PROJET\CDP\BenchMark\bq_read_write_analytics_v8.py", line 425, in plot_read_write_ratio_analysis
-    ax4.bar(x + i*width*2, read_vals, width, label=f'{tech} READ', alpha=0.7)
-  File "C:\Users\U50HO63\PROJET\CDP\BenchMark\bench\Lib\site-packages\matplotlib\__init__.py", line 1524, in inner
-    return func(
-           ^^^^^
-  File "C:\Users\U50HO63\PROJET\CDP\BenchMark\bench\Lib\site-packages\matplotlib\axes\_axes.py", line 2583, in bar
-    x, height, width, y, linewidth, hatch = np.broadcast_arrays(
-                                            ^^^^^^^^^^^^^^^^^^^^
-  File "C:\Users\U50HO63\PROJET\CDP\BenchMark\bench\Lib\site-packages\numpy\lib\_stride_tricks_impl.py", line 544, in broadcast_arrays
-    shape = _broadcast_shape(*args)
-            ^^^^^^^^^^^^^^^^^^^^^^^
-  File "C:\Users\U50HO63\PROJET\CDP\BenchMark\bench\Lib\site-packages\numpy\lib\_stride_tricks_impl.py", line 419, in _broadcast_shape
-    b = np.broadcast(*args[:32])
-        ^^^^^^^^^^^^^^^^^^^^^^^^
-ValueError: shape mismatch: objects cannot be broadcast to a single shape.  Mismatch is between arg 0 with shape (5,) and arg 1 with shape (4,).  
+  File "/usr/lib/spark/python/lib/pyspark.zip/pyspark/errors/exceptions/captured.py", line 185, in deco
+pyspark.errors.exceptions.captured.AnalysisException: [SCHEMA_NOT_FOUND] The schema `cacib-lsdh-dev-df`.`blmt_ds_lsdh_dev_ew9_bench_bl_ib_mg_tb` cannot be found. Verify the spelling and correctness of the schema and catalog.
+If you did not qualify the name with a catalog, verify the current_schema() output, or qualify the name with the correct catalog.
+To tolerate the error on drop use DROP SCHEMA IF EXISTS.
+25/08/31 10:06:28 INFO DataprocSparkPlugin: Shutting down dr
